24c79330051656486087be1c27fbb948a7e33359
razdvapoka <razdvapoka@gmail.com>
2019-04-17 19:38:42 +0400

    :construction: Draft grid recalculation feature

--- src/app.js
+++ src/app.js
@@ -1,11 +1,59 @@
-import React from 'react'
+import React, { useRef, useLayoutEffect, useState } from 'react'
 import { hot } from 'react-hot-loader/root'
 import './app.css'
+import data from './data.json'
 
-const App = () => (
-  <div className='app'>
-    <p>Hey!</p>
-  </div>
-)
+const App = ({
+  data,
+  initialImgWidth,
+  imgRatio
+}) => {
+  const [ imgWidth ] = useState(initialImgWidth)
+  const [ imagesPerPage, setImagesPerPage ] = useState(0)
+
+  const mainRef = useRef(null)
+
+  const recalcImagesPerPage = () => {
+    const {
+      offsetWidth: width,
+      offsetHeight: height
+    } = mainRef.current
+    const imagesPerRow = Math.floor(width / imgWidth)
+    const imgHeight = imgWidth / imgRatio
+    const rowCount = Math.floor(height / imgHeight)
+    setImagesPerPage(rowCount * imagesPerRow)
+  }
+
+  useLayoutEffect(() => {
+    window.addEventListener('resize', recalcImagesPerPage)
+    return () => {
+      window.removeEventListener('resize', recalcImagesPerPage)
+    }
+  })
+
+  useLayoutEffect(recalcImagesPerPage, [ mainRef.current ])
+
+  return (
+    <div className='app'>
+      <header />
+      <main className='main' ref={mainRef}>
+        {data.frames.slice(0, imagesPerPage).map(({ id, url }) => (
+          <img
+            key={id}
+            className='image'
+            src={url}
+            width={imgWidth}
+          />
+        ))}
+      </main>
+    </div>
+  )
+}
+
+App.defaultProps = {
+  data,
+  initialImgWidth: 300,
+  imgRatio: 960 / 540 // width / height
+}
 
 export default hot(App)
--- src/index.js
+++ src/index.js
@@ -7,4 +7,10 @@ import ReactDOM from 'react-dom'
 
 import App from './app'
 
-ReactDOM.render(<App />, document.getElementById('app'))
+// need this in dev mode only
+// because webpack inline styles are applied with a small delay
+// which makes it impossible to measure an element's height on its mounting
+window.setTimeout(
+  () => ReactDOM.render(<App />, document.getElementById('app')),
+  0
+)
