85b4f1fccd98c49e2e2f8e8473236e58a17b505e
razdvapoka <razdvapoka@gmail.com>
2019-04-17 16:44:34 +0400

    :tada: Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,106 @@
+const webpack = require('webpack')
+const path = require('path')
+const autoprefixer = require('autoprefixer')
+const precss = require('precss')
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: {
+    app: [ path.resolve(__dirname, 'src/index.js') ]
+  },
+
+  devtool: 'inline-source-map',
+
+  devServer: {
+    contentBase: path.resolve(__dirname, 'dist'),
+    hot: true
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+
+        include: [path.resolve(__dirname, 'src')],
+        loader: 'babel-loader',
+
+        options: {
+          plugins: ['syntax-dynamic-import'],
+
+          presets: [
+            [
+              '@babel/preset-env',
+              {
+                modules: false
+              }
+            ]
+          ]
+        }
+      },
+      {
+        test: /\.css$/,
+
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader',
+
+            options: {
+              importLoaders: 1,
+              sourceMap: true
+            }
+          },
+          {
+            loader: 'postcss-loader',
+
+            options: {
+              plugins: function () {
+                return [precss, autoprefixer]
+              }
+            }
+          }
+        ]
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+            options: { minimize: true }
+          }
+        ]
+      }
+    ]
+  },
+
+  output: {
+    chunkFilename: '[name].[hash].js',
+    filename: '[name].[hash].js'
+  },
+
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        vendors: {
+          priority: -10,
+          test: /[\\/]node_modules[\\/]/
+        }
+      },
+
+      chunks: 'async',
+      minChunks: 1,
+      minSize: 30000,
+      name: true
+    }
+  },
+
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/index.html',
+      filename: './index.html'
+    }),
+    new webpack.HotModuleReplacementPlugin()
+  ]
+}
